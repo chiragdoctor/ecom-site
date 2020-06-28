3d85793c7cea4973dafac128d2f8bb5fa10d270c
chiragdoctor <chirag.doctor@gmail.com>
2020-06-28 18:43:30 +0100

    user sign-in and sign-out routes

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { userSignupValidator } = require('../validator/index');
 
-const { signup } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 
 router.post('/signup', userSignupValidator, signup);
+
+router.post('/signin', signin);
+
+router.get('/signout', signout);
 
 module.exports = router;
