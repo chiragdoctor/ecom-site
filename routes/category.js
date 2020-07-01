284c984b4b9f28803e25cde5eacd1fe20b6ecbe3
chiragdoctor <chirag.doctor@gmail.com>
2020-07-01 20:10:32 +0100

    Category udpate, list, delete routes

--- routes/category.js
+++ routes/category.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 
-const { create, categoryById, read } = require('../controllers/category');
+const { create, categoryById, read, list, remove, update } = require('../controllers/category');
 const { userById } = require('../controllers/user');
 
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+
 router.get('/category/:categoryId', read);
+
+router.get('/categories', list);
 
 router.param('categoryId', categoryById);
 router.param('userId', userById);
