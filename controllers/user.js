0dc0f6dd372e2bf935024fd770c8faeed7a54662
chiragdoctor <chirag.doctor@gmail.com>
2020-07-08 20:41:27 +0100

    User read and update routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const User = require('../models/user');
 
 exports.userById = (req, res, next, id) => {
@@ -10,5 +11,24 @@ exports.userById = (req, res, next, id) => {
 
 		req.profile = user;
 		next();
+	});
+};
+
+exports.read = (req, res) => {
+	req.profile.hashed_password = undefined;
+	req.profile.salt = undefined;
+	return res.json(req.profile);
+};
+
+exports.update = (req, res) => {
+	User.findOneAndUpdate({ _id: req.profile._id }, { $set: req.body }, { new: true }, (err, user) => {
+		if (err) {
+			return res.status(400).json({
+				error: 'You are nor authorized to perform this action',
+			});
+		}
+		user.hashed_password = undefined;
+		user.salt = undefined;
+		res.json(user);
 	});
 };
