ccef34db259f6df44aa24f8694b048d94995c4e3
chiragdoctor <chirag.doctor@gmail.com>
2020-07-08 20:23:16 +0100

    product list by serach and product photo routes

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 
-const { create, productById, read, remove, update, list, listRelated, listCategories } = require('../controllers/product');
+const {
+	create,
+	productById,
+	read,
+	remove,
+	update,
+	list,
+	listRelated,
+	listCategories,
+	listBySearch,
+	photo,
+} = require('../controllers/product');
 const { userById } = require('../controllers/user');
 
 router.get('/product/:productId', read);
@@ -18,6 +29,10 @@ router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 
 router.get('/products/categories', listCategories);
+
+router.post('/products/by/search', listBySearch);
+
+router.get('/products/photo/:productId', photo);
 
 router.param('userId', userById);
 router.param('productId', productById);
